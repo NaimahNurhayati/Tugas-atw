80406fbda17a6e5359f5d2541398c9f14b698694
Naimah Nurhayati <70933572+NaimahNurhayati@users.noreply.github.com>
2020-11-10 14:11:31 +0700

    laravel model
    cara membuat model pada laravel

---
+++ system/app/Models/kategori.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Models;
+
+class kategori extends Model{
+	protected $table = 'kategori';
+}
---
+++ system/app/Models/produk.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Models;
+
+class produk extends Model {
+	protected $table = 'produk';
+}
